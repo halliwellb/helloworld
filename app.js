0c05e92f2f1e5f60860b6dd00df5d556cf2a7d59
Ben Halliwell <bkhalliwell@outlook.com>
2023-02-28 13:28:19 -0500

    new file:   app.js 	modified:   index.html 	modified:   style.css

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("Thank you for visiting!");
+let usersName = prompt("What is your name?");
+let usersFavoriteAnimal = prompt("What is your favorite animal?");
+console.log("Hi " + usersName + " welcome to my page!" + " I love " + usersFavoriteAnimal + "s" + "too!");
+document.write("Hi " + usersName + " welcome to my page!" + " I love " + usersFavoriteAnimal + "s" + " too!");
+document.write(" Let me know if you'd like more information about rewilding!");
+
+// if (usersName) == "Kassie") {
+//     alert("Great teaching!");
+//     console.log("Hi " + usersName + " Great teaching!");
+//     document.write("Hi " + usersName + " Great teaching!");
+// }
+// } else {
+//     alert("Thanks for coming!");
+// }
+
+
+// if {
+//     prompt("Would you like more information?");
+// } if ("Yes"){
+//     alert("Great!");
+// } else {
+//     alert("OK! Let us know if you change your mind!");
+// }
