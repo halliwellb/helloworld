21982fa177fb7ca6bb998d506ef566d896fb2808
Ben Halliwell <bkhalliwell@outlook.com>
2023-02-28 19:43:57 -0500

    modified:   app.js

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ if (usersFavoriteAnimal == "") {
   alert("Great choice!");
 }
 
-document.write("Hi, " + usersName + " welcome to my page! " + usersFavoriteAnimal + " are so cool! Let me know if you'd like more information about rewilding!");
-
 let userNum = prompt("Pick a number between 1 & 3 please")
 for (let i = 0; i < userNum; i++) {
   document.write("<img src='Mt.BlueResized.jpg' alt='' />");
@@ -28,3 +26,5 @@ let userGuess = prompt("Guess a number between 1 & 15");
 while (userGuess != myNumber) {
   userGuess = prompt("Wrong! Guess again. A number between 1 & 15");
 }
+
+document.write("Hi, " + usersName + " welcome to my page! " + usersFavoriteAnimal + " are so cool! Let me know if you'd like more information about rewilding!");
