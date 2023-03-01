682b473ccfa1669d27afd2dce1529eea4b59b025
Ben Halliwell <bkhalliwell@outlook.com>
2023-02-28 19:34:33 -0500

    renamed:    assets/Mt.BlueResized.jpg -> Mt.BlueResized.jpg 	modified:   app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 alert("Thank you for visiting!");
+let myNumber = 12;
+let userGuess = prompt("Guess a number between 1 & 15");
+
+while (userGuess !=myNumber){
+  userGuess = prompt("Wrong! Guess again. A number between 1 & 15");
+}
+
 let usersName = prompt("What is your name?");
 
 if (usersName == "") {
@@ -14,3 +21,8 @@ if (usersFavoriteAnimal == "") {
 }
 
 document.write("Hi, " + usersName + " welcome to my page! " + usersFavoriteAnimal + " are so cool! Let me know if you'd like more information about rewilding!");
+
+let userNum = prompt("Pick a number between 1 & 3 please")
+for (let i = 0; i < userNum; i++) {
+  document.write("<img src='Mt.BlueResized.jpg' alt='' />");
+}
