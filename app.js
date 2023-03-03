3eed8cdef6ea439630e47801a5364cf4fd3976da
Ben Halliwell <bkhalliwell@outlook.com>
2023-03-03 16:29:21 +0000

    Removed prompts for user ease of access

--- app.js
+++ app.js
@@ -1,48 +1,37 @@
-// alert("Thank you for visiting!");
 
-// let usersName = prompt("What is your name?");
-
-// if (usersName == "") {
-//   usersName = prompt("It would really enhance your enjoyment of the site if we knew!");
+// function helloUser() {
+//   alert("Thank you for visiting!");
+//   let usersName = prompt("What is your name?");
+//   if (usersName == "") {
+//     usersName = prompt("It would really enhance your enjoyment of the site if we knew!");
+//   } 
+//   return usersName;
 // }
 
-// let usersFavoriteAnimal = prompt("Hi, " + usersName + "! What's your favorite animal?");
-// if (usersFavoriteAnimal == "") {
-//   usersFavoriteAnimal = prompt(usersName + ", it honestly would enhance your enjoyment of the site!");
-// } else if (usersFavoriteAnimal == "Manatee") {
-//   alert("Manatees are my favorite too!");
-// } else {
-//   alert("Great choice!");
+// function userFavoriteAnimal(name){
+//   let usersFavoriteAnimal = prompt("Hi, " + name + "! What's your favorite animal?");
+//   if (usersFavoriteAnimal == "") {
+//     usersFavoriteAnimal = prompt(name + ", it honestly would enhance your enjoyment of the site!");
+//   } else if (usersFavoriteAnimal == "Manatee") {
+//     alert("Manatees are my favorite too!");
+//   } else {
+//     alert("Great choice!");
+//   }
+//   document.write("Hi, " + name + " welcome to my page! " + usersFavoriteAnimal + " are so cool! Let me know if you'd like more information about rewilding!");
+//   return usersFavoriteAnimal;
 // }
 
-function helloUser() {
-  alert("Thank you for visiting!");
-  let usersName = prompt("What is your name?");
-  if (usersName == "") {
-    usersName = prompt("It would really enhance your enjoyment of the site if we knew!");
-  }
-  let usersFavoriteAnimal = prompt("Hi, " + usersName + "! What's your favorite animal?");
-  if (usersFavoriteAnimal == "") {
-    usersFavoriteAnimal = prompt(usersName + ", it honestly would enhance your enjoyment of the site!");
-  } else if (usersFavoriteAnimal == "Manatee") {
-    alert("Manatees are my favorite too!");
-  } else {
-    alert("Great choice!");
-  }
-  document.write("Hi, " + usersName + " welcome to my page! " + usersFavoriteAnimal + " are so cool! Let me know if you'd like more information about rewilding!");
-}
+// function pickANumber(){
+//   let userNum = prompt("Pick a number between 1 & 3 please")
+//   for (let i = 0; i < userNum; i++) {
+//     document.write("<img src='Mt.BlueResized.jpg' alt='' />");
+//   }
 
-function pickANumber(){
-  let userNum = prompt("Pick a number between 1 & 3 please")
-  for (let i = 0; i < userNum; i++) {
-    document.write("<img src='Mt.BlueResized.jpg' alt='' />");
-  }
+//   let myNumber = 12;
 
-  let myNumber = 12;
+//   let userGuess = prompt("Guess a number between 1 & 15");
 
-  let userGuess = prompt("Guess a number between 1 & 15");
-
-  while (userGuess != myNumber) {
-    userGuess = prompt("Wrong! Guess again. A number between 1 & 15");
-  }
-}
+//   while (userGuess != myNumber) {
+//     userGuess = prompt("Wrong! Guess again. A number between 1 & 15");
+//   }
+// }
